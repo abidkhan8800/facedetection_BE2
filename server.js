564d5738955bcf75e653e422ee90df2ab4a2ed71
abidkhan8800 <khanabid844708@gmail.com>
2018-10-25 23:18:11 +0530

    h

--- server.js
+++ server.js
@@ -30,4 +30,4 @@ app.put('/image',(req, res) => { image.handleImage(req, res, db)});
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
 
-app.listen(process.env.PORT || 3001, ()=>{console.log('app is running on port ${process.env.PORT}')});
+app.listen(process.env.PORT || 3001, ()=>{console.log(`app is running on port ${process.env.PORT}`)});
